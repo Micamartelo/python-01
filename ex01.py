eaedbf9719886849d3295f31fe01c91a54a68324
Micaela <141280174+Micamartelo@users.noreply.github.com>
2023-08-22 10:27:01 -0300

    Add files via upload

---
+++ ex01.py
@@ -0,0 +1,10 @@
+n1 = int(input('um valor'))
+n2 = int(input('outro valor'))
+s = n1 + n2
+m = n1 - n2
+d = n1 * n2
+di = n1 / n2
+e = n1 ** n2
+l= n1 // n2
+k = n1 % n2
+print('A soma {} , a subtração {} , a mutiplicação {} , a divisão {} , a potencia {} , a divisão inteira {} , o resto da divisão {} ,'.format(s, m, d, di, e, l, k))
